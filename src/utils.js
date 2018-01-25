3447a190f41bd5afd0a412b7735af96063c3ee2a
Vinícius Pereira <vinicius.pereira@fastalelo.onmicrosoft.com>
2018-01-25 19:07:28 -0200

    GraphQL project with some queries and mutations

---
+++ src/utils.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+
+const APP_SECRET = 'GraphQL-is-aw3some'
+
+let getUserId = function (context) {
+    const Authorization = context.request.get('Authorization')
+
+    if (Authorization) {
+        const token = Authorization.replace('Bearer ', '')
+        const { userId } = jwt.verify(token, APP_SECRET)
+
+        return userId
+    }
+
+    throw new Error('Not authenticated')
+}
+
+module.exports = {
+    APP_SECRET,
+    getUserId
+}
